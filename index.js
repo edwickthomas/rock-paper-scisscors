0d6d31ded012b01aa32d29672c3970b884e6d850
edwickthomas <208377100+edwickthomas@users.noreply.github.com>
2025-06-09 22:18:28 +0530

    create playround function and playgame function

--- index.js
+++ index.js
@@ -6,27 +6,118 @@
 // else return paper
 
 function getComputerChoice(){
-    let rand = Math.random()*99
+    let rand = Math.random()*99;
     if (rand<=33){
-        return "rock"
+        return "rock";
     }
     else if ((rand<=66) && (rand>33)){
-        return "scissors"
+        return "scissors";
     }
     else {
-        return "paper"
+        return "paper";
     }
 }
-console.log(getComputerChoice())
 
 function getHumanChoice(){
     let hChoice= prompt("Please enter a value either rock, paper or scissors")
     return hChoice
 }
-console.log(getHumanChoice())
 let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice,computerChoice){
-    humanChoice = humanChoice.toLowerCase()
+    let hChoice = humanChoice.toLowerCase()
+    let combined = humanChoice+computerChoice
+    console.log("your pick is "+humanChoice+", The computer choice is "+computerChoice)
+    switch(combined){
+        case "rockscissors":
+            humanScore=humanScore+1
+            console.log("You win! rock beats scissors")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+        case "rockrock":
+            console.log("Tie! computer also picked rock")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+        case "rockpaper":
+            computerScore=computerScore+1
+            console.log("You lose! Paper Beats rock")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+        case "scissorsrock":
+            computerScore=computerScore+1
+            console.log("You lose! rock Beat scissors")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+
+        case "scissorspaper":
+            humanScore=humanScore+1
+            console.log("You win! scissors beats paper")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+        case "scissorsscissors":
+            console.log("Tie! computer also picked scissors")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+        case "paperrock":
+            humanScore=humanScore+1
+            console.log("You win! paper beats rock")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+
+
+        case "paperpaper":
+            console.log("Tie! computer also picked paper")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+        case "paperscissors":
+            computerScore=computerScore+1
+            console.log("You lose! Scissors Beat paper")
+            console.log("your score is "+ humanScore)
+            console.log("Computers score is "+computerScore)
+            break;
+    }
+}    
+function playGame(){
+    let i=5
+    
+    
+    switch (i){
+        case 5:
+            let humanSelection = getHumanChoice()
+            let computerSelection = getComputerChoice()
+            playRound(humanSelection,computerSelection)
+            i=i-1
+        case 4:
+            humanSelection = getHumanChoice()
+            computerSelection = getComputerChoice()
+            playRound(humanSelection,computerSelection)
+            i=i-1
+        case 3:
+            humanSelection = getHumanChoice()
+            computerSelection = getComputerChoice()
+            playRound(humanSelection,computerSelection)
+            i=i-1
+        case 2:
+            humanSelection = getHumanChoice()
+            computerSelection = getComputerChoice()
+            playRound(humanSelection,computerSelection)
+            i=i-1
+        case 1:
+            humanSelection = getHumanChoice()
+            computerSelection = getComputerChoice()
+            playRound(humanSelection,computerSelection)
+            i=i-1
+            break;                
+    }
 }
+playGame()
+
